8c176711c2cdc32621b255f76a46eda0f5e3fb42
unknown <rep141998@gmail.com>
2023-04-14 21:21:41 -0400

    Conexion a base de datos

---
+++ src/main/java/com/mycompany/lp2_proyecto1/alumnos.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lp2_proyecto1;
+
+/**
+ *
+ * @author Acer
+ */
+public class alumnos {
+    int id;
+    String nombre;
+    String apellido;
+    int edad;
+    String direccion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
